2a1ff3636b9083d1341c58ec1dae15c8df2a623e
JulienL79 <161200648+JulienL79@users.noreply.github.com>
2024-03-01 14:24:54 +0100

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,88 @@
+function typeWriterEffect(words, index = 0) {
+    const textElement = document.querySelector(".multiple-text");
+    const cursorElement = document.querySelector(".cursor");
+    const currentWord = words[index];
+    let i = 0;
+
+    // Fonction pour afficher chaque caractère avec un délai
+    function typeCharacter() {
+        if (i < currentWord.length) {
+            textElement.textContent += currentWord.charAt(i);
+            i++;
+            setTimeout(typeCharacter, Math.random() * 250 + 50); // Délai aléatoire pour l'effet de frappe
+        } else {
+            // Une fois que tous les caractères sont affichés, clignoter la barre verticale
+            cursorElement.style.display = "inline-block";
+            function blinkCursor() {
+                cursorElement.style.visibility = (cursorElement.style.visibility === "hidden") ? "visible" : "hidden";
+                setTimeout(blinkCursor, 500); // Délai pour le clignotement
+        }
+        blinkCursor();
+        }
+    }
+
+    // Fonction pour effacer le mot précédent
+    function eraseCharacter() {
+        if (i >= 0) {
+            textElement.textContent = currentWord.substring(0, i);
+            i--;
+            setTimeout(eraseCharacter, 100); // Délai pour l'effet d'effacement
+        } else {
+            cursorElement.style.display = "none"; // Cacher la barre verticale une fois que l'effacement est terminé
+            setTimeout(() => {
+                // Passer au mot suivant après un délai
+                typeWriterEffect(words, (index + 1) % words.length);
+            }, 500);
+        }
+    }
+
+    // Démarrer l'effet de frappe
+    typeCharacter();
+
+    // Effacer le mot actuel après un délai
+    setTimeout(() => {
+        eraseCharacter();
+        cursorElement.style.visibility = "visible";
+     }, currentWord.length * 100 + 1000); // Délai pour l'effacement
+}
+
+
+// Exemple d'utilisation avec des mots
+const words = ["Développeur Web", "Militaire", "Comptable"];
+typeWriterEffect(words);
+
+//pour faire apparaître les sections au fur et à mesure
+/*const sections = document.querySelectorAll('.section');
+
+window.addEventListener('scroll', () => {
+    sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+        if (sectionTop < windowHeight * 0.75) {
+            section.classList.add('actif');
+            // Réinitialiser les styles des sections qui ne sont pas actives
+            sections.forEach(otherSection => {
+                if (otherSection !== section) {
+                    otherSection.classList.remove('actif');
+                }
+            });
+        }
+    });
+});*/
+let sections = document.querySelectorAll('.section');
+
+  function checkViewport() {
+    sections.forEach(section => {
+      let sectionTop = section.getBoundingClientRect().top;
+      let sectionBottom = section.getBoundingClientRect().bottom;
+
+      // Check if section is in viewport
+      if (sectionTop < window.innerHeight && sectionBottom >= 0) {
+        section.classList.add('actif');
+      } else {
+        section.classList.remove('actif');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', checkViewport);
