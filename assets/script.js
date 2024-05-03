6ccc16132e3df7d71ea68b579893b1d5800e98dd
JulienL79 <161200648+JulienL79@users.noreply.github.com>
2024-05-03 11:13:36 +0200

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -134,3 +134,18 @@ const menuHamburger = document.querySelector(".menu-hamburger")
               navLinks.classList.remove('mobile-menu');
           });
         });
+
+window.onscroll = function() {scrollFunction()};
+
+        function scrollFunction() {
+          if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("scrollTopBtn").style.display = "block";
+          } else {
+            document.getElementById("scrollTopBtn").style.display = "none";
+          }
+        }
+        
+        function scrollToTop() {
+          document.body.scrollTop = 0;
+          document.documentElement.scrollTop = 0;
+        }
